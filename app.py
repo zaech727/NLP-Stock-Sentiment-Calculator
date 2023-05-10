1fff14986563f98072e4eee6e627cd446e45019d
raffi <rafaelbiedron2026@u.northwestern.edu>
2023-05-10 17:34:34 -0500

    starting webpage and connecting function

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
-
+web_scraper = WebScraper()
 
 @app.route("/", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
-        selected_option = request.form["dropdown"]
-        return f"You selected {selected_option}."
-    return render_template("index.html")
+        selected_option = request.json["dropdown"]
+        prediction = web_scraper.get_prediction(selected_option)
+        return jsonify({"prediction": prediction})
+    return render_template("home.html")
 
+@app.route("/get_prediction", methods=["POST"])
+def get_prediction():
+    selected_option = request.json["dropdown"]
+    prediction = web_scraper.get_prediction(selected_option)
+    return jsonify({"prediction": prediction})
 
 if __name__ == "__main__":
     app.run(debug=True)
