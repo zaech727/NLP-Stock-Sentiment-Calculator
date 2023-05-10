77e8d116da9ecf28a785740da95981a983aecac5
wsmartf <wjsmart12@gmail.com>
2023-05-10 17:09:38 -0500

    Initial flask setup, home page

--- app.py
+++ app.py
@@ -2,9 +2,14 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/home')
-def login():
-    return render_template('home.html')
 
-if __name__ == '__main__':
-    app.run()
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if request.method == "POST":
+        selected_option = request.form["dropdown"]
+        return f"You selected {selected_option}."
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
