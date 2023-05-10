8d9a707b885367bf8538f97faf975e9552676171
raffi <rafaelbiedron2026@u.northwestern.edu>
2023-05-10 17:52:46 -0500

    Solving 405 error

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template, request, jsonify
+import web_scraper
+import sentiment
 
 app = Flask(__name__)
-web_scraper = WebScraper()
+sentiment = sentiment.SentimentAnalysis()
 
 @app.route("/", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
         selected_option = request.json["dropdown"]
-        prediction = web_scraper.get_prediction(selected_option)
+        prediction = sentiment.get_prediction(get_headlines(selected_option))
         return jsonify({"prediction": prediction})
     return render_template("home.html")
 
